5ef52b075ddc263bd4c3bb7ee368c24925871bc8
slpraveenath <praveenath@datamining.com.sg>
2019-12-22 22:54:38 +0530

    Bug fix

--- R/Data Preprocessing/data_preprocessing.R
+++ R/Data Preprocessing/data_preprocessing.R
@@ -6,7 +6,7 @@ dataset$Age = ifelse(is.na(dataset$Age),
                      ave(dataset$Age, FUN = function(x) mean(x, na.rm = TRUE)),
                      dataset$Age)
 
-dataset$Salary = ifelse(is.na(dataset$Salary),
+    dataset$Salary = ifelse(is.na(dataset$Salary),
                      ave(dataset$Salary, FUN = function(x) mean(x, na.rm = TRUE)),
                      dataset$Salary)
 
@@ -23,8 +23,8 @@ dataset$Purchased = factor(dataset$Purchased,
 library(caTools)
 set.seed(123)
 split = sample.split(dataset$Purchased, SplitRatio = 0.8)
-training_set = subset(dataset, split = TRUE)
-test_set = subset(dataset, split = FALSE)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
 
 
 # Feature scaling
