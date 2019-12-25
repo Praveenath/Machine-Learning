383689b1b8815712c920a076970b85dc9cdb7923
slpraveenath <praveenath@datamining.com.sg>
2019-12-25 12:33:20 +0530

    Add SLR

--- Python/Regression/Simple Linear Regression/simple_linear_regression.py
+++ Python/Regression/Simple Linear Regression/simple_linear_regression.py
@@ -13,12 +13,30 @@ y = dataset.iloc[:,-1].values
 
 #Spliting the dataset into Train and test 
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
 
-"""
-# Feature scalling 
-from sklearn.preprocessing import StandardScaler
-sc_X = StandardScaler()
-X_train = sc_X.fit_transform(X_train)
-X_test = sc_X.transform(X_test)
-"""
+# Fit the model with training set
+from sklearn.linear_model import LinearRegression
+
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+# Predict test data
+y_pred = regressor.predict(X_test)
+
+# Visualizing Trained data
+plt.scatter(X_train,y_train, c = 'red')
+plt.plot(X_train, regressor.predict(X_train), c = 'blue')
+plt.title('Salary vs Year of Experience(Training data)')
+plt.xlabel('Salary')
+plt.ylabel('Years of Experience')
+plt.show()
+
+
+# Visualizing Predicted data
+plt.scatter(X_test,y_test, c = 'red')
+plt.plot(X_train, regressor.predict(X_train), c = 'blue')
+plt.title('Salary vs Year of Experience (Test Data)')
+plt.xlabel('Salary')
+plt.ylabel('Years of Experience')
+plt.show()
