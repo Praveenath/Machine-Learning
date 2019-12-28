a2a351da1c9e96ac4602dc8e9f9af69a0d9f5ce2
slpraveenath <praveenath@datamining.com.sg>
2019-12-28 16:26:22 +0530

    Add Backward elimination

--- R/Regression/Multiple Linear Regression/multiple_linear_regression.R
+++ R/Regression/Multiple Linear Regression/multiple_linear_regression.R
@@ -26,3 +26,16 @@ y_pred = predict(regressor, newdata = test_set)
 # Bulid optimal model using Backward Elimination
 regressor = lm(formula = Profit ~ R.D.Spend + Administration + Marketing.Spend + State, 
                data = dataset)
+summary(regressor)
+
+regressor = lm(formula = Profit ~ R.D.Spend + Administration + Marketing.Spend, 
+               data = dataset)
+summary(regressor)
+
+regressor = lm(formula = Profit ~ R.D.Spend + Marketing.Spend, 
+               data = dataset)
+summary(regressor)
+
+regressor = lm(formula = Profit ~ R.D.Spend, 
+               data = dataset)
+summary(regressor)
